e3d92938fa0ca52a7e3c85b0b546ff67d5431387
Jinho <dfghcvb11@naver.com>
2023-02-01 17:09:48 +0900

    feat: 구글 계정 인증 구현

--- src/main/java/com/jinho/hellosecurity1/config/SecurityConfig.java
+++ src/main/java/com/jinho/hellosecurity1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jinho.hellosecurity1.config;
 
+import com.jinho.hellosecurity1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터 체인에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  // secured,  preAuthorize, postAuthorize 어노테이션을 활성화함.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PrincipalOauth2UserService principalOauth2UserService;
+
+    public SecurityConfig(
+        PrincipalOauth2UserService principalOauth2UserService) {
+        this.principalOauth2UserService = principalOauth2UserService;
+    }
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -27,11 +36,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
             .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
             .anyRequest().permitAll()
+
             .and()
             .formLogin()
             .loginPage("/loginForm")
             .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 진행한다.
-            .defaultSuccessUrl("/");
+            .defaultSuccessUrl("/")
+
+            .and()
+            .oauth2Login()
+            .loginPage("/loginForm") /* 구글 로그인 완료. (후처리 필요) */
+            .userInfoEndpoint()
+            .userService(principalOauth2UserService);
 //            .usernameParameter("nickname");
     }
 }
