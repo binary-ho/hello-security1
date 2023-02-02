67f990822207f83ed6ed064330ae3ae86ad1afda
Jinho <dfghcvb11@naver.com>
2023-02-02 16:49:35 +0900

    feat: 네이버 로그인 구현

--- src/main/java/com/jinho/hellosecurity1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/jinho/hellosecurity1/config/oauth/PrincipalOauth2UserService.java
@@ -3,9 +3,11 @@ package com.jinho.hellosecurity1.config.oauth;
 import com.jinho.hellosecurity1.config.auth.PrincipalDetails;
 import com.jinho.hellosecurity1.config.oauth.provider.FacebookUserInfo;
 import com.jinho.hellosecurity1.config.oauth.provider.GoogleUserInfo;
+import com.jinho.hellosecurity1.config.oauth.provider.NaverUserInfo;
 import com.jinho.hellosecurity1.config.oauth.provider.OAuth2UserInfo;
 import com.jinho.hellosecurity1.model.Member;
 import com.jinho.hellosecurity1.repository.MemberRepository;
+import java.util.Map;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -42,6 +44,8 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            oAuth2UserInfo = new NaverUserInfo((Map<String, Object>) oAuth2User.getAttributes().get("response"));
         } else {
             System.out.println("미지원 소셜 로그인입니다.");
             oAuth2UserInfo = null;
